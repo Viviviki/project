d5d3e364c6474068a3dff5a89bd3e75f842d8eca
Viviviki <viktor.avronin@yandex.ru>
2025-04-30 15:30:53 +0500

    Добавление изображения на пряжу

--- app1/app/urls.py
+++ app1/app/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from main import views
 
@@ -24,3 +26,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app1/main/models.py
+++ app1/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50, help_text='Введите название пряжи')
+    origin = models.CharField(max_length=50, help_text='Введите происхождение пряжи')
+    price = models.IntegerField(help_text='Введите цену пряжи')
+
+    photo = models.ImageField(blank=True, upload_to='images')
+
+    def __str__(self):
+       return self.my_field_name
+    
